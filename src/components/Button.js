72dfa80903b9bc76a620ff29d7097dabe3a99d68
lian-c <lian.chan@telus.com>
2023-01-10 13:44:41 -0800

    Add classNames to day list items

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ import "components/Button.scss";
 export default function Button(props) {
   let buttonClass = classNames('button', { " button--danger" : props.danger, " button--confirm": props.confirm})
 
-
+console.log("wow", buttonClass)
 
   return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
 }
