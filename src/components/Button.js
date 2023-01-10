a0c7ce427a43c696fb51b452c6a9d8ff71a6948c
lian-c <lian.chan@telus.com>
2023-01-10 13:07:03 -0800

    Create day list item

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,13 +4,9 @@ import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-  let clicked = props.onClick
-  
-  
-  
   let buttonClass = classNames('button', { " button--danger" : props.danger, " button--confirm": props.confirm})
 
 
 
-  return <button className={buttonClass} disabled={props.disabled} onClick={clicked}>{props.children}</button>;
+  return <button className={buttonClass} disabled={props.disabled} onClick={props.onClick}>{props.children}</button>;
 }
