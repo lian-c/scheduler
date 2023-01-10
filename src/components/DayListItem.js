dbcbe35e7f849cb8cf4d16ac3444428cb14ee2dd
lian-c <lian.chan@telus.com>
2023-01-10 13:59:18 -0800

    Test /edge cases for day list items

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,12 +4,13 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
 const dayClass = classNames('day-list__item', { "day-list__item--selected" : props.selected, " day-list__item--full": (props.spots === 0)})
-console.log("lalala", dayClass)
 
   return (
     <li className={dayClass}  onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      {props.spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+      {(props.spots > 1) && <h3 className="text--light">{props.spots} spots remaining</h3>}
+      {props.spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
     </li>
   );
 }
