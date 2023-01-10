8099cd9360f0718ef21be4fe81c3d052f3200e2b
lian-c <lian.chan@telus.com>
2023-01-10 12:10:24 -0800

    Update button and functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,16 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let clicked = props.onClick
+  
+  
+  
+  let buttonClass = classNames('button', { " button--danger" : props.danger, " button--confirm": props.confirm})
+
+
+
+  return <button className={buttonClass} disabled={props.disabled} onClick={clicked}>{props.children}</button>;
 }
