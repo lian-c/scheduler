824ca6e9121d4c9364ae7cd5af5a2b65fb4aba23
lian-c <lian.chan@telus.com>
2023-01-11 19:18:34 -0800

    Add confirm, error, show, status component and storybook

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import classNames from "classnames";
+
 import InterviewerListItem from "./InterviewerListItem";
 import 'components/InterviewerList.scss'
 
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,10 @@ import InterviewerList from "components/InterviewerList.jsx"
 import Appointment from "components/Appointment/index.jsx"
 import Header from "components/Appointment/Header.jsx" 
 import Empty from "components/Appointment/Empty.jsx" 
+import Show from "components/Appointment/Show.jsx" 
+import Confirm from "components/Appointment/Confirm.jsx" 
+import Status from "components/Appointment/Status.jsx" 
+import Error from "components/Appointment/Error.jsx" 
 
 storiesOf("Button", module)
   .addParameters({
@@ -144,3 +148,7 @@ storiesOf("Button", module)
       .add("Appointment with Time", () => <Appointment time="12pm" />)   
       .add("Header", () => <Header time="12pm"/>)
       .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+      .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+      .add("Confirm", () => <Confirm message="Delete the appointment?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+      .add("Status", () => <Status message="Deleting"/>)
+      .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")}/>)
