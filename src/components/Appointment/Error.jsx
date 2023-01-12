ec01382312698be9f71008ac6ddd29a3755d3880
lian-c <lian.chan@telus.com>
2023-01-11 19:20:56 -0800

    Add form component

--- src/components/Appointment/Error.jsx
+++ src/components/Appointment/Error.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default function Header(props) {
+export default function Error(props) {
 
   return (
 <main className="appointment__card appointment__card--error">
--- src/components/Appointment/Status.jsx
+++ src/components/Appointment/Status.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 
-export default function Header(props) {
+export default function Status(props) {
 
   return (
 <main className="appointment__card appointment__card--status">
