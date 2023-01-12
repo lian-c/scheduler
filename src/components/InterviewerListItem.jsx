ae57206764de12b83c84865caa2f05ed8b2f2ca0
lian-c <lian.chan@telus.com>
2023-01-11 17:16:20 -0800

    Add functionality to interview list and items

--- src/components/InterviewerListItem.jsx
+++ src/components/InterviewerListItem.jsx
@@ -3,16 +3,17 @@ import  'components/InterviewerListItem.scss'
 import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
-
+//props is from index.stories
   const interviewClass = classNames("interviewers__item", {"interviewers__item--selected":props.selected})
 
 return (
-<li className={interviewClass} onClick={() => props.setInterviewer(props.name)}>
+<li className={interviewClass} onClick={props.setInterviewer}>
 <img
   className="interviewers__item-image"
   src={props.avatar}
   alt={props.name}
 />
+
 {props.selected && props.name}
 </li>);
 }
