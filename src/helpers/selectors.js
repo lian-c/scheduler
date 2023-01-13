633919f61fa504d6023c0590034bdc79eebb29c9
lian-c <lian.chan@telus.com>
2023-01-13 02:02:21 -0800

    Create helper func for get interviewers

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,46 +3,71 @@ const state = {
     {
       id: 1,
       name: "Monday",
-      appointments: [1, 2, 3]
+      appointments: [1, 2, 3],
     },
     {
       id: 2,
       name: "Tuesday",
-      appointments: [4, 5]
-    }
+      appointments: [4, 5],
+    },
   ],
   appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
+    1: { id: 1, time: "12pm", interview: null },
+    2: { id: 2, time: "1pm", interview: null },
+    3: {
       id: 3,
       time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
+      interview: { student: "Archie Cohen", interviewer: 2 },
     },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
+    4: { id: 4, time: "3pm", interview: null },
+    5: {
       id: 5,
       time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
+      interview: { student: "Chad Takahashi", interviewer: 2 },
+    },
+  },
+  interviewers: {
+    "1": {  
+      "id": 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
     }
   }
 };
 
 const getAppointmentsForDay = (state, day) => {
-
-const result = []
-state.days.map(days => {
-  if (days.name === day) {
-  const apptArray = days.appointments;
-    for(let id of apptArray){
-      result.push(state.appointments[id])
+  const result = [];
+  state.days.map((days) => {
+    if (days.name === day) {
+      const apptArray = days.appointments;
+      for (let id of apptArray) {
+        result.push(state.appointments[id]);
+      }
+      return result;
     }
-    return result
-  }
-})
-return result;
+  });
+  return result;
 };
 
-// console.log(getAppointmentsForDay(state,"Monday"))
-module.exports = { getAppointmentsForDay };
+const getInterview = (state, interview) => {
+  let result = null;
+  
+    if (interview){
+    (Object.values(state.interviewers)).map((interviewId) => {
+      if (interviewId.id === interview.interviewer) {
+        result = { ...interview, "interviewer": interviewId };
+        return result;
+      }
+    })
+  }
+  return result;
+  };
 
+console.log(getInterview(state, state.appointments["3"]["interview"]));
+
+module.exports = { getAppointmentsForDay, getInterview };
