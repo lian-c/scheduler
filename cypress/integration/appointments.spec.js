53d32c8b70dd1e05de646bf2862843772556f08d
lian-c <lian.chan@telus.com>
2023-01-19 21:35:31 -0800

    Add cypress and navigation & appointments test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,44 @@
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
+   });
+
+  xit("should book an interview", () => {
+    cy.get("[class=appointment__add-button]")
+    .first()
+    .click()
+    cy.get('[data-testid="student-name-input"]').type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+  
+  xit("should edit an interview", () => {
+    cy.get('.appointment__card')
+    .first().find("[alt='Edit']").click({force:true});
+    cy.get('[data-testid="student-name-input"]').clear().type("New Student");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "New Student");
+cy.contains(".appointment__card--show", "Tori Malcolm");
+
+  })
+
+  it("should delete an interview", () => {
+    cy.get("[alt='Delete']").click({force:true});
+    cy.contains("Confirm").click();
+    cy.contains("Deleting").should("exist");
+  cy.contains("Deleting").should("not.exist");
+
+  cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
+
+
+  })
+
+});
--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,23 +4,30 @@ import classNames from "classnames";
 
 export default function DayListItem(props) {
   //props passed from application
-const dayClass = classNames('day-list__item', { "day-list__item--selected" : props.selected, " day-list__item--full": (props.spots === 0)})
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    " day-list__item--full": props.spots === 0,
+  });
 
-const formatSpots = (spots) => {
-  return( 
-    <>
-  {spots === 0 && <h3 className="text--light">no spots remaining</h3>}
-  {(spots > 1) && <h3 className="text--light">{spots} spots remaining</h3>}
-  {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
-  </>
-  )
-}
+  const formatSpots = (spots) => {
+    return (
+      <>
+        {spots === 0 && <h3 className="text--light">no spots remaining</h3>}
+        {spots > 1 && <h3 className="text--light">{spots} spots remaining</h3>}
+        {spots === 1 && <h3 className="text--light">1 spot remaining</h3>}
+      </>
+    );
+  };
 
   return (
-    <li className={dayClass}  onClick={() => props.setDay(props.name)} selected={props.selected} data-testid="day">
+    <li
+      className={dayClass}
+      onClick={() => props.setDay(props.name)}
+      selected={props.selected}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
-      {formatSpots(props.spots)}
-     
+       {formatSpots(props.spots)}
     </li>
   );
 }
